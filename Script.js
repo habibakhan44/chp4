cc3da017089baf9dccfb9c11b5ecdf06b5a6a49b
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2024-08-12 01:33:45 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,30 @@
+var age=19 ,birthyear=2005 , number=10;
+document.write("Age:", age );
+document.write("Birth Year:", birthyear);
+document.write("Number:", number);
+
+
+
+// Legal Variable Names
+// userName
+// age_24
+// totalAmount
+// isValid
+// $price
+
+
+// Illegal Variable Names
+// 1stPlace (starts with a number)
+// user-name (contains a hyphen, which is not allowed)
+// const (reserved keyword)
+// function (reserved keyword)
+// my variable (contains a space)
+
+document.write("<h2>Rules for naming JS variables</h2>");
+document.write("Variable names can only contain ,numbers, $ and _. For example: $my_1stVariable<br><br>");
+
+        document.write("Variables must begin with a letter, $ or _. For example: $name, _name, or name<br><br>");
+
+        document.write("Variable names are case sensitive.<br><br>");
+
+        document.write("Variable names should not be JS reserved keywords.");
